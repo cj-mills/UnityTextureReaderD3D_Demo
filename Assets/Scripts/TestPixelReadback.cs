2a256637a1060cdb7cc20bafa7d9eee0f64cb85a
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-05 21:19:50 -0700

    Update TestPixelReadback.cs

--- Assets/Scripts/TestPixelReadback.cs
+++ Assets/Scripts/TestPixelReadback.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityTextureReaderD3D;
+using CJM.UnityTextureReaderD3D;
 
 public class TestPixelReadback : MonoBehaviour
 {
